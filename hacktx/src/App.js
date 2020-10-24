f1b4d3fc9d144b3907423ca9f33bb80f338791f9
Ted Kahl <tedkahl@gmail.com>
2020-10-24 11:57:44 -0700

    prototype addpantry using database

--- hacktx/src/App.js
+++ hacktx/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import components
-import Homepage from './components/Homepage'
-import About from './components/About'
-
+import Homepage from "./components/Homepage";
+import About from "./components/About";
+import About from "./components/AddPantry";
 
 function App() {
   return (
     <Router>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/about" component={About}/>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/add" component={AddPantry} />
     </Router>
   );
 }
