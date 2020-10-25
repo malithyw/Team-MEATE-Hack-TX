084a2bc501e11ec944938bab8d37c5ba153dd9f0
Ted Kahl <tedkahl@gmail.com>
2020-10-24 17:59:10 -0700

    update homepage for map

--- hacktx/src/components/Homepage.js
+++ hacktx/src/components/Homepage.js
@@ -1,11 +1,13 @@
-import react from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-dom";
 import { db } from "../firebase";
+import SimpleMap from "./Map";
 
 function Homepage(props) {
   return (
     <div>
       <h1>This is the homepage</h1>
+      <SimpleMap center={{ lat: 30.23532, lng: -97.72787 }} />
     </div>
   );
 }
